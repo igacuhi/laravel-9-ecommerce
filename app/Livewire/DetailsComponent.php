bf4b3ae3e641ac6c0d27f38610f5f9096c6de27f
igacuhi <igacuhi@gmail.com>
2024-01-08 18:38:10 +0300

    create product details page

--- app/Livewire/DetailsComponent.php
+++ app/Livewire/DetailsComponent.php
@@ -2,16 +2,23 @@
 
 namespace App\Livewire;
 use Livewire\Component;
+use App\Models\Product;
+
 
 class DetailsComponent extends Component
 {
+    public $slug;
+    public $product;
     public function mount($slug)
     {
         $this->slug = $slug;
+        $this->product = Product::where('slug', $this->slug)->first();
+
     }
     public function render()
     {
-        $product = Product::where('slug',$this->slug)->first();
-        return view('livewire.details-component',['product'=>$product]);
+        return view('livewire.details-component', ['product' => $this->product]);
+
     }
+    
 }
