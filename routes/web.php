840b20e3ef23dc7c6db79b3f9ff00d4e432978a0
igacuhi <igacuhi@gmail.com>
2024-01-08 14:28:45 +0300

    admin and user authentication:create routes for admin and user dashboard livewire components

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Livewire\User\UserDashboard;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\CartComponent;
 use App\Livewire\CheckoutComponent;
 use App\Livewire\ShopComponent;
 use App\Livewire\HomeComponent;
+use App\Livewire\Admin\AdminDashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,9 +28,15 @@ use App\Livewire\HomeComponent;
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
+Route::middleware(['auth'])->group(function(){
+    Route::get('/user/dashboard',UserDashboard::class)->name('user.dashboard');
+});
+Route::middleware(['auth','authadmin'])->group(function(){
+    Route::get('/admin/dashboard',AdminDashboard::class)->name('admin.dashboard');
+});
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
