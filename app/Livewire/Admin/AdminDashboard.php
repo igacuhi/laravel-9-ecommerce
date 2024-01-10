398a25369ba90ae3acafbfde5e0527c7f81f6b2a
igacuhi <119714279+igacuhi@users.noreply.github.com>
2024-01-10 23:00:12 +0300

    Update AdminDashboard.php

--- app/Livewire/Admin/AdminDashboard.php
+++ app/Livewire/Admin/AdminDashboard.php
@@ -5,7 +5,7 @@ namespace App\Livewire\Admin;
 use Livewire\Component;
 
 class AdminDashboard extends Component
-{
+{    //component logic
     public function render()
     {
         return view('livewire.admin.admin-dashboard')->layout('layouts.app');
