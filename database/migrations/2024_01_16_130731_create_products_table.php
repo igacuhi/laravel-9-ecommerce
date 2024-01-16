3dc3a87ec6d36e91df456aecb6426de6adb046c4
igacuhi <igacuhi@gmail.com>
2024-01-16 19:52:57 +0300

    making shop page products dynamic:add columns to categories and products table

--- database/migrations/2024_01_16_130731_create_products_table.php
+++ database/migrations/2024_01_16_130731_create_products_table.php
@@ -22,12 +22,12 @@ return new class extends Migration
             $table->string('SKU');
             $table->enum('stock_status',['instock','outofstock']);;
             $table->boolean('featured')->default(false);
-            $table->unsignedInterger('quantity')->default(10);
+            $table->unsignedInterger()->default(10);
             $table->string('image');
             $table->text('images');
             $table->bigInterger('category_id')->unsigned()->nullable();
             $table->timestamps();
-            $table->foreign('category_id')->reference('id')->on('categories')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
