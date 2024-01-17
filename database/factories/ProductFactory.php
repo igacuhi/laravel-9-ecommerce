dcc102a6979dec145d99f21636fbe017f9ce9649
igacuhi <igacuhi@gmail.com>
2024-01-17 22:46:00 +0300

    making shop page products dynamic:database/factories/categoryfactory.php     database/factories/productfactory.php  =>modifying this files

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -14,11 +14,22 @@ class ProductFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    
+
     public function definition(): array
     {
+        $product_name = $this->faker()->unique()->words($nb=6,$asText = true);
+        $slug = Str::slug($product_name,' -');
         return [
-            //
+            'name' => $product_name,
+            'slug' => $slug,
+            'short_description'=>$this->faker->text(200),
+            'description' => $this->faker->text(500),
+            'regular_price' => $this->faker->numberBetween(10,500),
+            'SKU' => 'PRD' , $this->faker->unique()->numberBetween(100,500),
+            'stock_status' => 'instock',
+            'Quantity' => $this->faker->numberBetween(10,50),
+            'image' => 'product'.this->faker->numberBetween(1,16),
+            'category_id' => $this->faker->numberBetween(1,5)
         ];
     }
 }
