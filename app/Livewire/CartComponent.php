854b413d48f102031f3aca58b5999bac499a6554
igacuhi <igacuhi@gmail.com>
2024-01-31 01:54:01 +0300

    update cart quantity:cart component blade.php

--- app/Livewire/CartComponent.php
+++ app/Livewire/CartComponent.php
@@ -13,6 +13,12 @@ class CartComponent extends Component
         $qty = $product->qty + 1;
         Cart::update($rowId,$qty);
     }
+    public function decreaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty - 1;
+        Cart::update($rowId,$qty);
+    }
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.app');
