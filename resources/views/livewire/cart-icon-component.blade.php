1f6686a513369d64a17c8770f8fdff5986e6bbb6
igacuhi <igacuhi@gmail.com>
2024-01-31 03:24:39 +0300

    making the cart icon dynamic

--- resources/views/livewire/cart-icon-component.blade.php
+++ resources/views/livewire/cart-icon-component.blade.php
@@ -1,3 +1,46 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
-</div>
+                            <div class="header-action-icon-2">
+                                    <a class="mini-cart-icon" href="{{route('shop-cart')}}">
+                                        <img alt="cart" src="{{asset('assets/imgs/theme/icons/icon-cart.svg')}}">
+                                        @if(Cart::count() > 0)
+                                             <span class="pro-count blue">{{Cart::count()}}</span>
+                                        @endif
+                                    </a>
+                                    <div class="cart-dropdown-wrap cart-dropdown-hm2">
+                                        <ul>
+                                            @foreach(Cart::content() as $item)
+                                            <li>
+                                                <div class="shopping-cart-img">
+                                                    <a href="product-details.html"><img alt="Surfside Media" src="assets/imgs/shop/thumbnail-3.jpg"></a>
+                                                </div>
+                                                <div class="shopping-cart-title">
+                                                    <h4><a href="product-details.html">Daisy Casual Bag</a></h4>
+                                                    <h4><span>1 × </span>$800.00</h4>
+                                                </div>
+                                                <div class="shopping-cart-delete">
+                                                    <a href="#"><i class="fi-rs-cross-small"></i></a>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="shopping-cart-img">
+                                                    <a href="product-details.html"><img alt="Surfside Media" src="assets/imgs/shop/thumbnail-2.jpg"></a>
+                                                </div>
+                                                <div class="shopping-cart-title">
+                                                    <h4><a href="product-details.html">Corduroy Shirts</a></h4>
+                                                    <h4><span>1 × </span>$3200.00</h4>
+                                                </div>
+                                                <div class="shopping-cart-delete">
+                                                    <a href="#"><i class="fi-rs-cross-small"></i></a>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="shopping-cart-footer">
+                                            <div class="shopping-cart-total">
+                                                <h4>Total <span>$4000.00</span></h4>
+                                            </div>
+                                            <div class="shopping-cart-button">
+                                                <a href="cart.html" class="outline">View cart</a>
+                                                <a href="checkout.html">Checkout</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
