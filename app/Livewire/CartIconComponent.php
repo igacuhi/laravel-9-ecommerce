317c91c21b3e05c3b5837bae5def773dd43f35b9
igacuhi <igacuhi@gmail.com>
2024-01-31 03:10:32 +0300

    making the cart icon dynamic:make the livewire view

---
+++ app/Livewire/CartIconComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CartIconComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.cart-icon-component');
+    }
+}
