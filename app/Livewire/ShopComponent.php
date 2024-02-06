fced07736b56db53e24dae4dcb15ee0ae0987232
igacuhi <igacuhi@gmail.com>
2024-02-06 15:37:29 +0300

    editing search item functionality

--- app/Livewire/ShopComponent.php
+++ app/Livewire/ShopComponent.php
@@ -43,6 +43,16 @@ class ShopComponent extends Component
         Cart::instance('wishlist')->add($product_id,$product_name,1,$product_price)->associate('\App\Models\Product');
         $this->emitTo('wishlist-icon-component','refreshComponent');
     }
+     
+    public function remmoveFromWishlist($product_id)
+    {
+        foreach(Cart::instance('wishlist')->content() as $witem)
+        {
+            Cart::instance('wishlist')->remove($witem->rowId);
+            $this->emitTo('wishlist-icon-component','refreshComponent');
+            return;
+        }
+    }
 
     public function render()
     {
