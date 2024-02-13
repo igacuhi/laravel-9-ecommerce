2f55302bb40b7d44f6a2ca38308c07a5d3c1c783
igacuhi <igacuhi@gmail.com>
2024-02-13 14:11:32 +0300

    admin delete category

--- resources/views/livewire/admin/admin-categories-component.blade.php
+++ resources/views/livewire/admin/admin-categories-component.blade.php
@@ -70,15 +70,15 @@
             </div>
         </section>
     </main>
-    <div class="model" id="deleteConfirmation">
-        <div class="model-dialog">
-            <div class="model-content">
-                <div class="model-body pb-30 pt-30">
+    <div class="modal" id="deleteConfirmation">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body pb-30 pt-30">
                     <div class="row">
                         <div class="col-md-12 text-center">
                             <h4 class="pb-3">Do you want to delete this record?</h4>
-                            <button type="button" class="btn btn-secondary" data-bs-toggle="model" data-bs-target="#deleteConfirmation">Cancel</button>
-                            <button type="button" class="btn-btn-danger" onclick="deleteCategory()">Delete</button>
+                            <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#deleteConfirmation">Cancel</button>
+                            <button type="button" class="btn btn-danger" onclick="deleteCategory()">Delete</button>
                          </div>                       
                     </div>
                 </div>
